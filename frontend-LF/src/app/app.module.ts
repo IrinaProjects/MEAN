900f3b3c34a970f68dcccf519c341ce047d05b18
Irina Spasova <irina.spasova.81@gmail.com>
2022-12-19 21:57:45 +0200

    header ad footer ready

--- frontend-LF/src/app/app.module.ts
+++ frontend-LF/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { GroundZeroComponent } from './Shared/ground-zero/ground-zero.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+const material = [MatIconModule];
+
+const routes: Routes = [{ path: '', component: GroundZeroComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GroundZeroComponent,
   ],
   imports: [
-    BrowserModule
+    ...material,
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
